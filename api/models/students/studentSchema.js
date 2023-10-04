87ae1774cefa4f7d77c431ac836954cd77cc069c
CodeeUp <mertnode1@gmail.com>
2023-10-04 09:14:48 +0300

    ogrenci modelini bitirdik

--- api/models/students/studentSchema.js
+++ api/models/students/studentSchema.js
@@ -1,31 +1,41 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const studentSchema = mongoose.Schema({
-    name: {
-        type:String,
-        required:true
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  studentNo: {
+    type: Number,
+    required: true,
+  },
+  adress: {
+    type: String,
+  },
+  class: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Class",
+  },
+  addedUser: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  guardian: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Guardian",
+  },
+  exams: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Exams",
     },
-    surname: {
-        type:String,
-        required:true
-    },
-    studentNo: {
-        type:Number,
-        required:true
-    },
-    adress: {
-        type:String,
-    },
-    class: {
-        type: mongoose.Schema.ObjectId,
-        ref:"Class"
-    },
-    addedUser: {
-        type: mongoose.Schema.ObjectId,
-        ref:"User"
-    },
-    guardian: {
-        type: mongoose.Schema.ObjectId,
-        ref:"Guardian"
-    },
-})
+  ],
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
